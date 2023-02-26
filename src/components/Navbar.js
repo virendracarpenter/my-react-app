b714f83542dfebdf5b8de62537f4ea4dd0195f6c
Virendra Carpenter <viren.28opensky@gmail.com>
2023-02-26 15:33:39 +0530

    changed title in nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import logo from "../images/logo.svg";
 function NavBar() {
   return (
     <nav>
-      <img src={logo} alt="react logo" className="nav-logo"/>
+      <img src={logo} alt="react logo" className="nav-logo" />
       <h3 className="nav-logo-title">ReactFacts</h3>
-      <h4 className="nav-title">React Course - Project 1</h4>
+      <h4 className="nav-title">React - Project 1</h4>
     </nav>
   );
 }

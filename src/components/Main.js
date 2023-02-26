f2764f9e251ed905ed8d742bb453413d1f7fb4d5
Virendra Carpenter <viren.28opensky@gmail.com>
2023-02-26 18:09:09 +0530

    new changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,13 @@
 function Main() {
   return (
-    <main className="Main">
-      <h1>Main</h1>
+    <main>
+      <h1 className="main-title">Fun Facts about React</h1>
+      <ul className="main-title-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands enterprise apps, including mobile apps</li>
+      </ul>
     </main>
   );
 }
